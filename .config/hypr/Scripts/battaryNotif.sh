96aea3429c8bc6e9344e5cf7f9769fcdc650a27a
lidawn <contact@loayidwan.com>
2025-05-10 15:17:41 +0000

    Updated low battary notif. script

--- .config/hypr/Scripts/battaryNotif.sh
+++ .config/hypr/Scripts/battaryNotif.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 BATTERY_PATH="/org/freedesktop/UPower/devices/battery_BAT1"
 
-upower --monitor-detail "$BATTERY_PATH" | while read -r line; do
-  if [[ "$line" =~ "percentage" ]] || [[ "$line" =~ "state" ]]; then
-    PERCENT=$(upower -i "$BATTERY_PATH" | grep "percentage" | awk '{print $2}' | tr -d '%')
-    STATE=$(upower -i "$BATTERY_PATH" | grep "state" | awk '{print $2}')
+upower --monitor-detail "$BATTERY_PATH" | \
+grep --line-buffered -E 'percentage|state' | \
+while read -r line; do
+  CURRENT_PERCENT=$(upower -i "$BATTERY_PATH" | grep "percentage" | awk '{print $2}' | tr -d '%')
+  CURRENT_STATE=$(upower -i "$BATTERY_PATH" | grep "state" | awk '{print $2}')
 
-    if [[ "$STATE" == "discharging" ]]; then
-      if (( PERCENT <= 10 )); then
-        notify-send -u critical "🔋 Battery Low" "${PERCENT}% remaining!"
-      fi
+  if [[ "$CURRENT_STATE" == "discharging" ]]; then
+    if (( CURRENT_PERCENT <= 10 )); then
+      notify-send -u critical "🔋 Battery Low" "${CURRENT_PERCENT}% remaining!"
     fi
   fi
 done
