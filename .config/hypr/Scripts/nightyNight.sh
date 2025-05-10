f39c06c4461ab86bd7476b608c6ad0d7eb04b520
lidawn <contact@loayidwan.com>
2025-05-10 12:54:15 +0000

    added hyprsunset on-click in waybar module

--- .config/hypr/Scripts/nightyNight.sh
+++ .config/hypr/Scripts/nightyNight.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-hyprsunset -t 4500
+# Check if hyprsunset is running
+if pgrep -x "hyprsunset" > /dev/null
+then
+    # If hyprsunset is running, kill it
+    pkill hyprsunset
+else
+    # If hyprsunset is not running, start it in the background
+    hyprsunset -t 4500 &
+fi
+
